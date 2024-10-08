a8301ab045f5a22f37eff01c21c48123e69d7cf2
yvan321 <neilyvancaliwan@gmail.com>
2024-10-08 14:50:14 +0800

    First Commit

---
+++ my-react-vite-app/src/sections/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import styles from '/my-react-vite-app/src/sections/Footer/FooterStyles.module.css';
+
+function Footer() {
+  return (
+    <section id="footer" className={styles.container}>
+      <p>
+        &copy; 2024 Harris johnsen. <br />
+        All rights reserved.
+      </p>
+    </section>
+  );
+}
+
+export default Footer;
